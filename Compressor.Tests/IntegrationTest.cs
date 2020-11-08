43cfb4679cca6551eb788025fb3dd64c964f572c
Roman Metlinskyi <metlinskyi@hotmail.com>
2020-11-08 11:57:43 +0100

    Hello World!

---
+++ Compressor.Tests/IntegrationTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Compressor.Tests
+{
+    [TestClass]
+    public class IntegrationTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
